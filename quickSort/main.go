d30296a49bcd47b1da5ba08244be488b6c620809
shayan vosoughi <shayanwqhw@gmail.com>
2024-01-01 07:02:39 +0330

    add quicksort

---
+++ quickSort/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type arrayType []int
+
+func quickSort(arr arrayType) arrayType {
+	if len(arr) <= 2 {
+		// array with 2 elements is already sorted
+		return arr
+	} else {
+		//pivot is middle element of array
+		pivot := arr[len(arr)/2]
+		var less, greater, equal arrayType
+
+		for _, value := range arr {
+			if value < pivot {
+				less = append(less, value)
+			} else if value > pivot {
+				greater = append(greater, value)
+			} else {
+				equal = append(equal, value)
+			}
+		}
+
+		return append(append(quickSort(less), equal...), quickSort(greater)...)
+	}
+}
+
+func main() {
+	arr := arrayType{5, 3, 30, 1, 19, 2}
+	sorted := quickSort(arr)
+	fmt.Println(sorted)
+}
